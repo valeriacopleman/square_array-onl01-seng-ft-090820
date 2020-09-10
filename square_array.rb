4c2f3f85f2ac96f87f5e3712ef6d779009313e5c
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 17:26:42 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
   array = [1,2,3]
-  array.each do |square|
-  
-  square =square**2
   new_numbers=[]
+  array.each do |square|
+  square =square**2
+
   new_numbers<< square 
-  return new_numbers
+  puts new_numbers
 end
 end
