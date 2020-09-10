f982bb232af3b750f74951a33d110128dbabaea1
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 17:44:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,15 +1,16 @@
 def square_array(array)
   array = [1,2,3]
   numbers=[]
-  new_numbers =[9,10,16,25]
-  more_new_numbers=[]
-  
   array.each do |square|
   numbers<< square = square**2
 end
 return numbers 
-  #new_numbers.each do |square|
-    #more_new_numbers<< square = square**2 
-#end
-#return more_new_numbers
+
+def square_array(new_numbers)
+    new_numbers =[9,10,16,25]
+  more_new_numbers=[]
+  new_numbers.each do |square|
+  more_new_numbers<< square = square**2 
+end
+return more_new_numbers
 end
