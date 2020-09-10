37768a329f493f16d8fd4ba48815110f7a918345
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 17:45:51 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,11 +6,3 @@ def square_array(array)
 end
 return numbers 
 
-def more_array(new_numbers)
-    new_numbers =[9,10,16,25]
-  more_new_numbers=[]
-  new_numbers.each do |hop|
-  more_new_numbers<< hop = hop**2 
-end
-return more_new_numbers
-end
