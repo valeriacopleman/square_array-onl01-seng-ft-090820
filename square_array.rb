13019a27262a34ef3e7074a5187f40e619e71b46
Valeria  Copleman <valeriacopleman@gmail.com>
2020-09-10 17:27:25 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,8 @@ def square_array(array)
   array = [1,2,3]
   new_numbers=[]
   array.each do |square|
-  square =square**2
 
-  new_numbers<< square 
+  new_numbers<< square = square**2
   puts new_numbers
 end
 end
